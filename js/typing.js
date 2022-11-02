26309b87260352bf124369038378110e856ec49f
Nonwhiskerscat <nonwhiskerscat@gmail.com>
2022-11-03 08:36:00 +0900

    ver3.1

--- js/typing.js
+++ js/typing.js
@@ -1,23 +1,45 @@
-window.addEventListener('load',function(){
-    var typingBool = false; 
-    var typingIdx=0; 
+window.addEventListener('load', function(){
+    var typeText = document.querySelector("h2#type");
+    var textToBeTypedArr = 
+    ["호텔의 품격", 
+    "DIGNITY OF HOTEL", 
+    "ホテルの品格", 
+    "飯店的品格", 
+    "qualité hôtelière", 
+    "calidad hotelera", 
+    "deversorium qualis",
+    "qualità alberghiera",
+    "Hotelqualität",
+    "chất lượng khách sạn",
+    "คุณภาพโรงแรม",
+    "جودة الفندق"
+    ];
+    var index = 0, isAdding = true, textToBeTypedIndex = 0;
     
-    var typingTxt = $(".typing-txt").text(); 
-    
-    typingTxt=typingTxt.split(""); 
-    
-    if(typingBool==false){ 
-        typingBool=true;     
-        var tyInt = setInterval(typing,100); 
-    } 
+    function playAnim() {
+    setTimeout(function () {
+        typeText.innerText = textToBeTypedArr[textToBeTypedIndex].slice(0, index)
+        if (isAdding) {
+        if (index > textToBeTypedArr[textToBeTypedIndex].length) {
+            isAdding = false
+            setTimeout(function () {
+            playAnim()
+            }, 2000)
+            return
+        } else {
+            index++
+        }
+        } else {
+            if (index === 0) {
+            isAdding = true
+            textToBeTypedIndex = (textToBeTypedIndex + 1) % textToBeTypedArr.length
+            } else {
+            index--
+            }
+        }
+        playAnim()
+        }, isAdding ? 120 : 60)
+    }
+    playAnim()
+});
 
-    function typing(){ 
-        if(typingIdx<typingTxt.length){ 
-        $(".typing").append(typingTxt[typingIdx]);
-        typingIdx++; 
-        } else{ 
-        clearInterval(tyInt); 
-        } 
-    }  
-}
-);
