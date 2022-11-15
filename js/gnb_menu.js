772cd4a9f8470c8605587521622545de4e3a2c98
Nonwhiskerscat <nonwhiskerscat@gmail.com>
2022-11-15 11:40:19 +0900

    ver18.5

--- js/gnb_menu.js
+++ js/gnb_menu.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $('.menu_modal').hide();
     $('.menu').click(function() {
-        $('.menu_modal').stop().slideDown();
-        $('header').stop().fadeOut();
+        $('.menu_modal').stop().slideDown(1000);
+        $('header').stop().fadeOut(1000);
         
         $('body').on('scroll touchmove mousewheel', function(event) {
             event.preventDefault();
@@ -15,8 +15,8 @@ $(document).ready(function() {
 
 
         function close_menu() {
-            $('.menu_modal').stop().slideUp();
-            $('header').stop().fadeIn();
+            $('.menu_modal').stop().slideUp(1000);
+            $('header').stop().fadeIn(1000);
             $('body').off('scroll touchmove mousewheel');
         }
 
