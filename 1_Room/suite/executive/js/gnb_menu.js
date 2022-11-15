364138eec19aade1cc300cbdd6dedc204728a64a
Nonwhiskerscat <nonwhiskerscat@gmail.com>
2022-11-15 14:15:02 +0900

    ver18.6.1

---
+++ 1_Room/suite/executive/js/gnb_menu.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $('.menu_modal').hide();
+    $('.menu').click(function() {
+        $('.menu_modal').stop().slideDown(1000);
+        $('header').stop().fadeOut(1000);
+
+        const openModal = (e) => {
+            document.body.style.overflow = "hidden";
+        };
+
+        const closeModal = (e) => {
+            document.body.style.overflow = "unset";
+         };
+        
+        $('body').on('scroll touchmove mousewheel', function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+                return false;
+            });
+        });
+
+
+        function close_menu() {
+            $('.menu_modal').stop().slideUp(1000);
+            $('header').stop().fadeIn(1000);
+            $('body').off('scroll touchmove mousewheel');
+        }
+
+        $('.menu_close').click(close_menu);
+    });
+
+    
+
+
+
